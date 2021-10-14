fb7b30a0bae167fef8bba7e19a3a96840fb58a75
Shiba Vijay Minmule <89532983+Sheeba02M@users.noreply.github.com>
2021-10-14 03:00:30 -0700

    Use of this() to achieve Constructor Chaining

---
+++ ConstructorChaining.java
@@ -0,0 +1,23 @@
+public class ConstructorChaining
+{
+    ConstructorChaining()
+    {
+        this(2);
+        System.out.println("Default constructor of class.");
+    }
+    ConstructorChaining(int x)
+    {
+        System.out.println("Parameterized (1 parameter) constructor of class.");
+        System.out.println("The value of x is "+x);
+    }
+    ConstructorChaining(int x, int y)
+    {
+        this();
+        System.out.println("Parameterized (2 parameters) constructor of class.");
+        System.out.println("The  value of x and y is  " + x + " and " + y + ". " + "The sum of x and y is " + (x + y) );
+    }
+    public static void main(String... a)
+    {
+        ConstructorChaining obj = new ConstructorChaining(2 , 27);
+    }
+}
